bef6b83b3487650b0c1f177aeccae3474fce6351
chinonso-ibiam <nmosafe@gmail.com>
2026-08-04 10:13:20 +0100

    Delete Python Projects/Telecom Customer Churn EDA/Customer_churn.py

--- Python Projects/Telecom Customer Churn EDA/Customer_churn.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-df = pd.read_csv('Customer_churn.Csv')
-sns.countplot(x='Churn', data=df)
-plt.title("Customer Churn Distribution")
-plt.show()
